302fd5fcf570748deceb4e33f95cdba3026a2086
Danila Uprivanov <mr.uprivanov@yandex.ru>
2023-12-17 20:52:47 +0300

    add user registered event

---
+++ src/NightTasker.Common.Messaging/NightTasker.Common.Messaging/Events/IUserRegistered.cs
@@ -0,0 +1,11 @@
+﻿namespace NightTasker.Common.Messaging.Events;
+
+/// <summary>
+/// Событие регистрации пользователя.
+/// </summary>
+public interface IUserRegistered
+{
+    public Guid Id { get; }
+
+    public string UserName { get; set; }
+}
